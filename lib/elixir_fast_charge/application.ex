e6b798791207a20747ed47020b036173d2b0c8a1
Belen Seoane <seoane.m.b@gmail.com>
2025-06-11 00:15:31 -0300

    init app, supervisor and finder

---
+++ lib/elixir_fast_charge/application.ex
@@ -0,0 +1,14 @@
+defmodule ElixirFastCharge.Application do
+
+  use Application
+
+  # @impl true
+  def start(_type, _args) do
+    children = [
+      {ElixirFastCharge.Finder, []}
+    ]
+
+    opts = [strategy: :one_for_one, name: ElixirFastCharge.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
